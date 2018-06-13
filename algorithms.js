705e2cb624eb73695d42e1c3a3f0d11a413e4036
Allan Liebold <allan.liebold@gmail.com>
2018-06-12 20:23:03 -0700

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -1,8 +1,10 @@
 
 function fizzBuzz(num) {
   for(var i = 0; i < num; i++) {
-    if(i % 2 === 0) {
-      console.log('fizz');  
+    if(i % 3 === 0 && i % 5 ===0) {
+      console.log('fizzbuzz');  
+    } else if(i % 3 === 0) {
+      console.log('fizz'); 
     }
   }
 }
