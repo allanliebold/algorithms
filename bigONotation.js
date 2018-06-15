873bda4f99f1748146d28bb6ce02cae0c77def58
Allan Liebold <allan.liebold@gmail.com>
2018-06-15 06:06:01 -0700

    Update bigONotation.js

--- bigONotation.js
+++ bigONotation.js
@@ -14,5 +14,9 @@ function logAll(array) {
 
 // Exponential runtime - Big O Notation "O(n^2)"
 function addAndLog(array) {
-  
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      console.log(array[i] + array[j]); 
+    }
+  }
 }
