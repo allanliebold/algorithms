3634c8c70a43ec53502d22b6ade3ad6c3a02403b
Allan Liebold <allan.liebold@gmail.com>
2018-06-19 06:11:06 -0700

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,6 +2,10 @@ function isPalindrome(string) {
   string = string.toLowerCase();
   var charactersArr = string.split('');
   var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  
+  charactersArr.forEach(char => {
+    
+  });
 }
 
 isPalindrome("Madam I'm Adam");
