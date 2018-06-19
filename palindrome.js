96017941466f849b45ae2ec1e9d396c862976562
Allan Liebold <allan.liebold@gmail.com>
2018-06-19 06:13:56 -0700

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -7,6 +7,9 @@ function isPalindrome(string) {
   charactersArr.forEach(char => {
     if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
   });
+  
+  if (lettersArr.join('') === lettersArr.reverse().join('')) return true;
+  else return false;
 }
 
 isPalindrome("Madam I'm Adam");
