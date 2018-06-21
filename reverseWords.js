83f78f324ff50c33e707dc9d2a63c0f633a757e8
Allan Liebold <allan.liebold@gmail.com>
2018-06-21 05:24:35 -0700

    Update reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -1,6 +1,9 @@
 function reverseWords(string) {
   // return string with each word reversed
   var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+  
+  
 }
 
 reverseWords('this is a string'); // should return 'siht si a gnirts'
