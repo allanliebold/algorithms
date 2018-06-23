ef1db446332064b210825b7e4b08986e62a3d28f
Allan Liebold <allan.liebold@gmail.com>
2018-06-23 16:44:03 -0700

    Update reverseArr.js

--- reverseArr.js
+++ reverseArr.js
@@ -1,5 +1,5 @@
 function reverseArrayInPlace(arr) {
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 0; i < arr.length / 2; i++) {
     var tempVar = arr[i];
     arr[i] = arr[arr.length - 1 - i];
     arr[arr.length - 1 - i] = tempVar;
