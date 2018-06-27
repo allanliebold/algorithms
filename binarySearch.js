e45f50b421e9df1f0634c553874b539bb2e10f01
Allan Liebold <allan.liebold@gmail.com>
2018-06-27 06:26:56 -0700

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -6,4 +6,7 @@ function binarySarch(numArray, key) {
   else if (middleElem < key && numArray.length > 1) {
     return binarySearch(numArray.splice(middleIdx, numArray.length));
   }
+  else if (middleElem > key && numArray.length > 1) {
+    
+  }
 }
