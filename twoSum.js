2ad6d87dfd02bbf82d2b74b2c537212a6b164701
Allan Liebold <allan.liebold@gmail.com>
2018-06-27 06:13:45 -0700

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,7 @@
 function twoSum(numArray, sum) {
   // Returns every pair of numbers from 'numArray' that adds up to the 'sum'
   var pairs = [];
+  var hashtable = [];
   
   for (var i = 0; i < numArray.length; i++) {
     var currNum = numArray[i];
