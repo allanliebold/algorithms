419e9356d7dc214ba3305d35851f9fe6f1069067
Allan Liebold <allan.liebold@gmail.com>
2018-06-29 06:04:25 -0700

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -17,6 +17,9 @@ function fibMemo(index, cache) {
   if (cache[index]) return cache[index];
   else {
     if (index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);  
+    }
   }
 }
 
