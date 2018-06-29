f0bb6e89092f85becb038dfaf5a5697f84268a17
Allan Liebold <allan.liebold@gmail.com>
2018-06-29 06:09:09 -0700

    Update sieveOfEratosthenes.js

--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -1,5 +1,9 @@
 function sieveOfEratosthenes(num) {
   // return all prime numbers up to 'num' in an array
+  var primes = [];
+  for (var i = 0; i <= num; i++) {
+    primes[i] = true;  
+  }
 }
 
 sieveOfEratosthenes(20); // should return [2, 3, 5, 7, 11, 13, 17, 19]
