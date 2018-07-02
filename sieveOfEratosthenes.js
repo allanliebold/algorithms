f15754b914dfba28cdf2c0d788b619027cbf3e49
Allan Liebold <allan.liebold@gmail.com>
2018-07-02 07:29:45 -0700

    Update sieveOfEratosthenes.js

--- sieveOfEratosthenes.js
+++ sieveOfEratosthenes.js
@@ -7,6 +7,10 @@ function sieveOfEratosthenes(num) {
   
   primes[0] = false;
   primes[1] = false;
+  
+  for (var i = 2; i <= Math.sqrt(num); i++) {
+    
+  }
 }
 
 sieveOfEratosthenes(20); // should return [2, 3, 5, 7, 11, 13, 17, 19]
