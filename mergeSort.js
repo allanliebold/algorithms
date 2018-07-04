62e380b32a10c3c396b388c15aac7f18b9c979db
Allan Liebold <allan.liebold@gmail.com>
2018-07-04 11:37:28 -0700

    Update mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,5 +1,7 @@
 function mergeSort(arr) {
-  
+  var middleIndex = Math.floor(arr.length / 2);
+  var firstHalf = arr.slice(0, middleIndex);
+  var secondHalf = arr.slice(middleIndex); 
 }
 
 function merge(array1, array2) {
