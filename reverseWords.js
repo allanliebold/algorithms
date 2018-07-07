c2240506f58856cfd92127cbf065c7aff1eff4be
Allan Liebold <allan.liebold@gmail.com>
2018-07-07 05:18:17 -0700

    Update reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -7,11 +7,9 @@ function reverseWords(string) {
     var reversedWord = '';
     for (var i = word.length - 1; i >= 0; i--) {
       reversedWord += word[i]; 
-    }
-    
+    }    
     reversedWords.Arr.push(reversedWord);
   });
-  
   return reversedWordsArr.join(' ');
 }
 
