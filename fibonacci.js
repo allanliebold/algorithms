7a12066c7a090e8ddba21b746669f3643906c315
Allan Liebold <allan.liebold@gmail.com>
2018-10-16 13:47:33 -0700

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -10,6 +10,7 @@ function fibonacci(position) {
 fibonacci(4); // returns 3
 // return fibonacci(3) + fibonacci(2)
 // fibonacci(3) return fibonacci(2) + fibonacci(1) 
+// fibonacci(2) less than 3. return 1
 
 fibonacci(6); // returns 8
 // return fibonacci(5) + fibonacci(4)
