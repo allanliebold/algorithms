80f3d74838d4cad4993a68828d172b9e83e8d2f1
Allan Liebold <allan.liebold@gmail.com>
2018-10-16 09:21:03 -0700

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -8,6 +8,9 @@ function fibonacci(position) {
 }
 
 fibonacci(6); // returns 8
+// return fibonacci(5) + fibonacci(4)
+// fibonacci(5) - return fibonacci(4) + fibonacci(3)
+//
 fibonacci(7); // returns 13
 
 // Memoized Fibonacci
