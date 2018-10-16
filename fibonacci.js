a2970395f34fdad86dafc3855467f2bcbf6db212
Allan Liebold <allan.liebold@gmail.com>
2018-10-16 06:13:12 -0700

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,11 @@
 // Fibonacci Sequence - 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
 
 function fibonacci(position) {
-  if (position < 3) return 1; 
-  else return fibonacci(position - 1) + fibonacci(position - 2); 
+  if (position < 3) {
+    return 1;
+  } else {
+    return fibonacci(position - 1) + fibonacci(position - 2); 
+  }
 }
 
 fibonacci(6); // returns 8
